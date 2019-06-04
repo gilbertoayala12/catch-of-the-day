52194d1bb867ea0c4644342b07487afd7d630ae9
gilberto ayala <gilbertoayala12@gmail.com>
2019-06-04 14:41:59 -0700

    adding a form to add fishes to the inventory, learning about state and passing methods using props

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,30 @@
 import React from "react";
-import Header from "./Header"
-import Order from "./Order"
-import Inventory from "./Inventory"
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = fish => {
+    // 1. Take a copy of the existing state
+    const fishes = { ...this.state.fishes };
+    // 2. add our new fish to that fishes variable to use incremental keys
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. set the new fishes object to state
+    this.setState({
+      fishes: fishes
+    }); // this only updates a piece of state instead the whole of it 
+  };
   render() {
     return (
       <div className="catch-of-the-day ">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
         </div>
-        <Order></Order> 
-        <Inventory></Inventory>
+        <Order />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
