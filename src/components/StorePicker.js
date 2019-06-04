72641f49d2ee61f80df63156c204b30270db6287
gilberto ayala <gilbertoayala12@gmail.com>
2019-06-04 12:19:16 -0700

    understading events, ref and binding and rerouting the homepage to the storepage using the data in the form without reloading the page

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,21 +1,36 @@
 import React, { Fragment } from "react";
-import {getFunName} from '../helpers';
+import { getFunName } from "../helpers";
 // siempre importar react en los componentes
 class StorePicker extends React.Component {
   // se escribe asi la clase para la reutilizacion
   // todas necesitan un metodo de awebo y es el render
   // nunca se toca el DOM hasta que se monta toda la aplicacion en la pagina
   // u cant return children elements
+  myInput = React.createRef();
+
+  goToStore = event => {
+    // stop the form from submitting
+    event.preventDefault();
+    // get the text from that input
+
+    const storeName = this.myInput.current.value;
+    // change page to store/loquemetieron usando react router
+    this.props.history.push(`/store/${storeName}`);
+  };
   render() {
     return (
-      <Fragment>
-        <form className="store-selector">
-          {/* Comentarios, siempre dentro del return element */}
-          <h2>Please enter a store </h2>
-          <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
-          <button type="submit">Visit Store</button>
-        </form>
-      </Fragment>
+      <form className="store-selector" onSubmit={this.goToStore}>
+        {/* Comentarios, siempre dentro del return element */}
+        <h2>Please enter a store </h2>
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store ☻</button>
+      </form>
     );
   }
 }
