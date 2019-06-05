6a0cb87f15afa682513cc0ebb3425e32e8b7930b
gilberto ayala <gilbertoayala12@gmail.com>
2019-06-05 13:43:54 -0700

    Adding fishes to order and showing price and qty of the ordered fish, also if the fish is not available the order updates the price and shows a message saying sorry :cold_sweat:

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,10 +1,44 @@
 import React from "react";
-
+import { formatPrice } from "../helpers";
 class Order extends React.Component {
-  render() {
+  renderOrder = key => {
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+    const isAvailable = fish.status === "available";
+    if (!isAvailable) {
+      return (
+        <li key={key}>
+          Sorry {fish ? fish.name : "fish"} is no longer available{" "}
+        </li>
+      );
+    }
     return (
-      <div className="order">
-      Order 
+      <li key={key}>
+        {count} lbs {fish.name}
+        {formatPrice(count * fish.price)} 🐟
+      </li>
+    );
+  };
+  render() {
+    const orderId = Object.keys(this.props.order);
+    const total = orderId.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key];
+      const count = this.props.order[key];
+      const available = fish && fish.status === "available";
+      if (available) {
+        return prevTotal + count * fish.price;
+      } else {
+        return prevTotal;
+      }
+    }, 0);
+    return (
+      <div className="order-wrap">
+        <h2>Order</h2>
+        <ul className="order">{orderId.map(this.renderOrder)}</ul>
+        <div className="total">
+          Total:
+          <strong>{formatPrice(total)}</strong>
+        </div>
       </div>
     );
   }
