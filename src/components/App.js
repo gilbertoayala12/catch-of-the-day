18e76e535df486a95f3cd106933ab0c99b6a5907
gilberto ayala <gilbertoayala12@gmail.com>
2019-06-05 12:57:11 -0700

    adding fishes to order complete, learned about using the key of a product as a prop

--- src/components/App.js
+++ src/components/App.js
@@ -22,8 +22,17 @@ class App extends React.Component {
 
   loadSampleFishes = () => {
     this.setState({
-        fishes: sampleFishes
+      fishes: sampleFishes
     });
+  };
+
+  addToOrder = key => {
+    //1 take a copy of state
+    const order = { ...this.state.order };
+    //2 either add to the order or update the number in our order
+    order[key] = order[key] + 1 || 1;
+    //3 call setState to update our state object
+    this.setState({ order: order });
   };
   render() {
     return (
@@ -31,12 +40,21 @@ class App extends React.Component {
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
-           {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}></Fish>)}
-          
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}
           </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,17 +1,25 @@
 import React from "react";
-import {formatPrice} from "../helpers";
+import { formatPrice } from "../helpers";
 class Fish extends React.Component {
-  render() {
 
-    const {image, name, desc, price, status} = this.props.details; // es6 deconstructed shizzle
+  handleClick = () => {
+    this.props.addToOrder(this.props.index);
+  };
+  render() {
+    const { image, name, desc, price, status } = this.props.details; // es6 deconstructed shizzle
+    const isAvailable = status === "available";
+
     return (
       <li className="menu-fish">
         <img src={image} alt={name} />
-        <h3 className="fish-name">{name}
-        <span className="price">{formatPrice(price)}</span> 
+        <h3 className="fish-name">
+          {name}
+          <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add to cart 🛒 </button>
+        <button disabled={!isAvailable} onClick={this.handleClick}>
+          {isAvailable ? "Add to cart 🛒" : "Sold Out!"}
+        </button>
       </li>
     );
   }
