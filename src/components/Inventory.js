f8059b98bb813831c7eea521433e30f27cbf1df9
gilberto ayala <gilbertoayala12@gmail.com>
2019-06-17 12:09:17 -0700

    Setting up firebase OAuth

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,18 +1,34 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import firebase from "firebase";
+import Login from "./Login";
 import AddFishForm from "./AddFishForm";
 import EditFishForm from "./EditFishForm";
+import { firebaseApp } from "../base";
 
 class Inventory extends React.Component {
-  static propTypes ={
-    addFish: PropTypes.func, 
+  static propTypes = {
+    addFish: PropTypes.func,
     deleteFish: PropTypes.func,
     loadSampleFishes: PropTypes.func,
     updateFish: PropTypes.func,
-    fishes: PropTypes.object,
-
-  }
+    fishes: PropTypes.object
+  };
+  authHandler = async authData => {
+    // 1. Look up the current store in the firebase database 
+    // 2. Claim it if there's no owner
+    // 3. Set the state of the inventory component to reflect the current user
+    console.log(authData);  
+  };
+  authenticate = provider => {
+    const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+    firebaseApp
+      .auth()
+      .signInWithPopup(authProvider)
+      .then(this.authHandler);
+  };
   render() {
+    return <Login authenticate={this.authenticate} />;
     return (
       <div className="inventory">
         <h2>Inventory</h2>
@@ -22,7 +38,7 @@ class Inventory extends React.Component {
             index={key}
             fish={this.props.fishes[key]}
             updateFish={this.props.updateFish}
-            deleteFish = {this.props.deleteFish}
+            deleteFish={this.props.deleteFish}
           />
         ))}
         <AddFishForm addFish={this.props.addFish} />
